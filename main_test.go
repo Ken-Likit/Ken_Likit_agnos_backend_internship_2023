276761f2362f1af60f92ae94e0324963336a2b82
Ken-Likit <88905005+Ken-Likit@users.noreply.github.com>
2023-05-30 02:07:09 -0500

    Update main_test.go
    Updated main_test.go to fix test

--- main_test.go
+++ main_test.go
@@ -1,25 +1,26 @@
 package main
 
-import "testing"
+import ("testing")
 
 type PasswordTest struct {
 	arg1 string; expected int
 }
 
 var pwTest = []PasswordTest{
-	PasswordTest{"aA1", 3},
-	PasswordTest{"1445D1cd", 0},
-	PasswordTest{"!ecse34dF", 0},
-	PasswordTest{"aaa124kebndlfhb", 2},
-	PasswordTest{"hello,test1444", 2},
-	PasswordTest{"a", 7},
-	PasswordTest{"WERWEDSAE314", 1},
-	PasswordTest{"ABCDEFGHIJKLMNOPQRSTUvwxyz1", 8},
+	{"aA1", 3},
+	{"1445D1cd", 0},
+	{"!ecse34dF", 0},
+	{"aaa124kebndlfhb", 1},
+	{"hello,test1444", 1},
+	{"a", 5},
+	{"WERWEDSAE314", 1},
+	{"ABCDEFGHIJKLMNOPQRSTUvwxyz1", 8},
+	{"1111111", 2},
 }
 
-func strongTest(t *testing.T) {
+func TestStrong(t *testing.T){
 
-	for _, test := range pwTest{
+    for _, test := range pwTest{
 		if output := strongPassword(test.arg1); output != test.expected {
 			t.Errorf("Output %q not equal to expected %q", output, test.expected)
 		}
